64f58f0adaab3efcb7fb950f8ecd5d0bf54c2a3a
cjx3m <cjlc@hotmail.com>
2021-10-07 19:56:03 -0700

    Math functionality

---
+++ mathChallenge.py
@@ -0,0 +1,40 @@
+# first_value = 5
+# second_value = 4
+
+# sum = first_value + second_value
+# difference = first_value - second_value
+# product = first_value * second_value
+# quotient = first_value / second_value
+# modulus = first_value % second_value
+# exponent = first_value ** second_value
+
+# print(f'Sum: {sum}')
+# print(f'Difference: {difference}')
+# print(f'Product: {product}')
+# print(f'Quotient: {quotient}')
+# print(f'Modulus: {modulus}')
+# print(f'Exponent: {exponent}')
+
+# pi = 3.14
+# print(type(pi))
+# print(int(pi))
+
+# uptime = 99.99
+# print(type(uptime))
+# print(int(uptime))
+
+# pi = 3.14
+# print(type(pi))
+# print(int(pi))
+# print(round(pi))
+
+# uptime = 99.99
+# print(type(uptime))
+# print(int(uptime))
+# print(round(uptime))
+
+first_value = round(7.654321, 2)
+print(first_value)
+
+second_value = round(9.87654, 3)
+print(second_value)
