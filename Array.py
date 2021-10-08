30e0fc01fee8e4f5cb6db8ffe487142058c5752c
cjx3m <cjlc@hotmail.com>
2021-10-08 13:39:48 -0700

    Arrays

---
+++ Array.py
@@ -0,0 +1,54 @@
+colors = ["red", "green", "blue", "yellow", "black", "white", "orange"]
+
+print(colors)
+# print(type(colors))
+
+# sundry = ['Jhon', 3.14, 7, False]
+
+# print(sundry)
+# print(type(sundry))
+
+# print(f'0-based indexing into the list ... second item: {colors[1]}')
+
+# print(f'Last item of the list: {colors[-1]}')
+
+# print(f'Next to last item of the list: {colors[-2]}')
+
+# print('Print a SLICE, starting at the index and excluding index 5')
+# print(colors[2:5])
+# print(type(colors[2:5]))
+
+# print('Print a SLICE, starting at index 0 to index 3')
+# print(colors[:3])
+
+# print('Print a SLICE, starting at index 4 to the end of the list')
+# print(colors[4:])
+
+# print('Print a SLICE, from the 4th from the end (but not the last item)')
+# print(colors[-4:-1])
+
+# colors.reverse()
+# print(colors)
+
+# colors.sort()
+# print(colors)
+
+# color = colors.pop(3)
+# print('popped', color)
+
+# print(colors)
+
+# colors.append('purple')
+# colors.append('magenta')
+
+# colors.remove('black')
+# colors.remove('white')
+
+# print(colors)
+
+# newColors = ['lime', 'gray']
+# colors.extend(newColors)
+
+colors.clear()
+
+print(colors)
